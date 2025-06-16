d704426b6044edcabd90de1d54674ac314e4b485
Daniel Yeung <dyeung520@pcbauto.ai>
2025-06-16 12:23:55 -0700

    Markdown function

--- opensipi/integrated_flows.py
+++ opensipi/integrated_flows.py
@@ -9,7 +9,11 @@ Last updated on Jan. 5, 2024
 Description:
     This module contains all top-level integrated flows.
 """
-
+"""
+Last modified: June 10, 2025 (Daniel Yeung)
+Description:
+    Changed report_dir to generate markdown instead of pdf
+"""
 
 from opensipi.sipi_infra import Platform
 
@@ -25,7 +29,8 @@ def sim2report(input_info, mntr_info):
     pf.drop_dsn_file(xtract_tool)
     sim_exec = pf.parser(pf.input_data)
     result_config_dir, report_config_dir = pf.run(sim_exec, mntr_info)
-    report_dir = pf.report(result_config_dir, report_config_dir)
+    # report_dir = pf.report(result_config_dir, report_config_dir)
+    report_dir = pf.report_markdown(result_config_dir, report_config_dir)
     return report_dir
 
 
